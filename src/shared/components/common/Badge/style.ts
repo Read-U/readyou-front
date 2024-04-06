b527472da6c1b4838bda772b9048c423fa7f6383
hyjoong <maplesosser@naver.com>
2024-04-07 01:53:58 +0900

    style: badge style 수정 - color, background-color 속성 값 수정

--- src/shared/components/common/Badge/style.ts
+++ src/shared/components/common/Badge/style.ts
@@ -1,23 +1,6 @@
 import { BadgeProps } from '@/shared/components/common/Badge';
 import styled, { css } from 'styled-components';
 
-const getBackgroundColor = (type: BadgeProps['type']) => {
-  switch (type) {
-    case 'optional':
-      return css`
-        background-color: ${(props) => props.theme.colors.gray};
-      `;
-    case 'required':
-      return css`
-        background-color: ${(props) => props.theme.colors.darkgray};
-      `;
-    default:
-      return css`
-        background-color: ${(props) => props.theme.colors.lightgray};
-      `;
-  }
-};
-
 export const Badge = styled.div<BadgeProps>`
   display: flex;
   justify-content: center;
@@ -28,6 +11,12 @@ export const Badge = styled.div<BadgeProps>`
   font-weight: 700;
   font-size: 1.6rem;
   padding: 0.25rem 0.5rem;
-  color: ${(props) => props.theme.colors.lightnavy};
-  ${(props) => getBackgroundColor(props.type)};
+  color: ${(props) =>
+    props.type === 'optional'
+      ? props.theme.colors.lightnavy
+      : props.theme.colors.lightgray};
+  background-color: ${(props) =>
+    props.type === 'optional'
+      ? props.theme.colors.gray
+      : props.theme.colors.navy};
 `;
