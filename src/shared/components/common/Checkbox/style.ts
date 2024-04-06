4cb6d05519c4740ccc7ba2f14a1cfa99fbe0eb38
hyjoong <maplesosser@naver.com>
2024-04-07 01:17:58 +0900

    feat: checkbox component 구현

---
+++ src/shared/components/common/Checkbox/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Checkbox = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export const CheckboxInput = styled.input`
+  width: 2rem;
+  height: 2rem;
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 50%;
+  border: 1px solid ${(props) => props.theme.colors.gray};
+  appearance: none;
+  transition: all 0.1s ease-in-out;
+  cursor: pointer;
+
+  &:checked {
+    background-image: url('/images/check.jpg');
+    background-size: 80%;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
