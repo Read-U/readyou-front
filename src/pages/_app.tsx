fd3afa42e40541dc220751491f7c6764b69b33e3
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-04-06 16:23:30 +0900

    design: styled-components theme 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from '@/styles/global-styles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
