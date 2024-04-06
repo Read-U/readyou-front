7330bc2fb2cc277ed0d5821caf772983c310c4c4
이시영 <swy05123@gmail.com>
2024-04-06 23:32:24 +0900

    feat: toggle버튼 구현

---
+++ src/shared/components/Preview/index.tsx
@@ -0,0 +1,9 @@
+const Preview = () => {
+    return (
+      <div>
+
+      </div>
+    );
+  };
+  
+  export default Preview;
---
+++ src/shared/types/toggleType.ts
@@ -0,0 +1,3 @@
+export interface toggleType {
+    isLightMode: boolean;
+}
