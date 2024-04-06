a2c7a8256b8f6c9da5783fa66ae91d97426ca0dc
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-04-06 22:41:57 +0900

    refactor: text 불러오는 로직 변경

--- src/shared/components/common/Button/index.tsx
+++ src/shared/components/common/Button/index.tsx
@@ -4,10 +4,13 @@ interface ButtonType {
   type?: 'back' | 'add';
 }
 
+const LABEL_TYPE = {
+  add: '추가',
+  back: '뒤로가기',
+};
+
 const Button = ({ type = 'add' }: ButtonType) => {
-  return (
-    <S.Button type={type}>{type === 'add' ? '추가' : '뒤로가기'}</S.Button>
-  );
+  return <S.Button type={type}>{LABEL_TYPE[type]}</S.Button>;
 };
 
 export default Button;
