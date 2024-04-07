4dcbe081c4542a8eceadb69aec54152e2a9f91e4
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-04-07 15:53:59 +0900

    feat: 이미지 파일 -> 링크로 변환 API 추가

--- src/shared/components/ImageCreateInput/index.tsx
+++ src/shared/components/ImageCreateInput/index.tsx
@@ -1,11 +1,51 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import * as S from './style';
 import UploadItem from '../common/UploadItem';
+import axios from 'axios';
 
 const ImageCreateInput = () => {
-  const [imageList, setImageList] = useState<string[]>([]);
+  const [imageNameList, setImageNameList] = useState<string[]>([]);
+  const [ImageLinkList, setImageLinkList] = useState<string[]>([]);
 
+  /** test */
+  useEffect(() => {
+    console.log(ImageLinkList);
+  }, [ImageLinkList]);
+
+  /** 이미지 -> 링크 변환 API */
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+
+    if (files && files.length > 0) {
+      const formData = new FormData();
+      formData.append('image', files[0]);
+
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: `${process.env.NEXT_PUBLIC_API_URL}/api/img`,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        data: formData,
+      };
+
+      axios
+        .request(config)
+        .then((response) => {
+          const imageLink = response.data.imageLink;
+          setImageLinkList((prevImageLinkList) => [
+            ...prevImageLinkList,
+            imageLink,
+          ]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const handleAddImage = (event: ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
     if (files && files.length > 0) {
       const fileName = files[0].name;
@@ -15,14 +55,16 @@ const ImageCreateInput = () => {
       if (!extension || !['jpg', 'png', 'jpeg'].includes(extension))
         return alert('이미지 타입이 아닙니다.');
 
-      setImageList((prevImageList) => [...prevImageList, fileName]);
+      handleFileChange(event);
+
+      setImageNameList((prevImageNameList) => [...prevImageNameList, fileName]);
     }
   };
 
   const handleItemDelete = (index: number) => {
     return () => {
-      const newList = imageList.filter((_, i) => i !== index);
-      setImageList(newList);
+      const newList = imageNameList.filter((_, i) => i !== index);
+      setImageNameList(newList);
     };
   };
 
@@ -35,13 +77,17 @@ const ImageCreateInput = () => {
         <S.FileHiddenButton
           type="file"
           id="input-file"
-          onChange={handleFileChange}
+          onChange={handleAddImage}
         />
       </S.RelativeBox>
-      {imageList.length > 0 && (
+      {imageNameList.length > 0 && (
         <S.BottomWrapper>
-          {imageList.map((list, index) => (
-            <UploadItem text={list} onClick={handleItemDelete(index)} />
+          {imageNameList.map((list, index) => (
+            <UploadItem
+              key={index}
+              text={list}
+              onClick={handleItemDelete(index)}
+            />
           ))}
         </S.BottomWrapper>
       )}
