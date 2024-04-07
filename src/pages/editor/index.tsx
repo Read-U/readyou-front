a63f0222c7410c9e6a8865ff0870e5677310dcfd
woo29 <dldngur37@naver.com>
2024-04-07 23:46:54 +0900

    feat: 메타 데이터 추가(OG, Twitter)

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -4,6 +4,7 @@ import Editor from '@/shared/components/Editor';
 import Preview from '@/shared/components/Preview';
 import { useRecoilState } from 'recoil';
 import { styled } from 'styled-components';
+import Head from 'next/head';
 
 const EditorPageContainer = styled.div`
   display: flex;
@@ -26,13 +27,49 @@ const EditorPage = () => {
   const [step, setStep] = useRecoilState(stepState);
 
   return (
-    <EditorPageContainer>
-      <EditorBox>
-        {step === 0 && <DefaultSetting />}
-        {step === 1 && <Editor />}
-      </EditorBox>
-      <Preview />
-    </EditorPageContainer>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
+        <title>Editor | READ YOU</title>
+        <meta name="title" content="READ YOU" />
+        <meta name="keyword" content="README, READYOU, 간편함, 프로젝트" />
+        <meta name="description" content="누구나 간편하게 README.md 작성하기" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* Open Graph */}
+        <meta property="og:title" content="READ YOU" />
+        <meta
+          property="og:description"
+          content="누구나 간편하게 README.md 작성하기"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="배포 링크" />
+        <meta property="og:image" content="/images/kakao.png" />
+        <meta property="og:image:alt" content="READ YOU 이미지" />
+        <meta property="og:locale" content="ko_KR" />
+
+        {/* Twitter */}
+        <meta property="twitter:title" content="READ YOU" />
+        <meta
+          name="twitter:description"
+          content="누구나 간편하게 README.md 작성하기"
+        />
+        <meta name="twitter:card" content="website" />
+        <meta property="twitter:site" content="배포 링크(추가 예정)" />
+        <meta name="twitter:image" content="/images/twitter.png" />
+        <meta property="twitter:image:alt" content="READ YOU 이미지" />
+      </Head>
+      <EditorPageContainer>
+        <EditorBox>
+          {step === 0 && <DefaultSetting />}
+          {step === 1 && <Editor />}
+        </EditorBox>
+        <Preview />
+      </EditorPageContainer>
+    </>
   );
 };
 
