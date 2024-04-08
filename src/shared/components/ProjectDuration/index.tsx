5d207a765d6d14fe60cf6feefa57d8c23d92bec5
이시영 <swy05123@gmail.com>
2024-04-08 13:19:03 +0900

    refactor: 기간 예외처리

--- src/shared/components/ProjectDuration/index.tsx
+++ src/shared/components/ProjectDuration/index.tsx
@@ -22,13 +22,34 @@ const ProjectDuration = () => {
     start: '',
     end: '',
   });
+  const validateDate = (dateString: string) => {
+    var regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    return regex.test(dateString);
+  };
 
   useEffect(() => {
     const newMarkdown = markdown.map((item: ItemProps) => {
       if (item.name === 'period' && item.detail) {
+        let [startDate, endDate] = item.detail.split('~');
+        console.log(item.detail, startDate, endDate);
         const dateRegex = /\b\d{4}.\d{2}.\d{2}\b/g;
-        const dates = item.detail.match(dateRegex);
-        if (dates !== null) setDuration({ start: dates[0], end: dates[1] });
+        const start = startDate ? startDate.match(dateRegex) : '';
+        const end = endDate ? endDate.match(dateRegex) : '';
+        console.log(start, end);
+
+        if (start && start[0] && end && end[0]) {
+          setDuration({ start: start[0], end: end[0] });
+          return;
+        }
+        if (start && start[0]) {
+          setDuration({ start: start[0], end: '' });
+          return;
+        }
+        if (end && end[0]) {
+          setDuration({ start: '', end: end[0] });
+          return;
+        }
+        setDuration({ start: '', end: '' });
       }
     });
   }, []);
@@ -37,6 +58,16 @@ const ProjectDuration = () => {
     e: ChangeEvent<HTMLInputElement>,
     type: DURATION_TYPE,
   ) => {
+    if (!validateDate(e.target.value)) {
+      if (type === 'start') {
+        setDuration({ start: '', end: duration.end });
+      }
+      if (type === 'end') {
+        setDuration({ start: duration.start, end: '' });
+      }
+      return;
+    }
+
     const date = e.target.value.split('-');
     const newFormDate = `${date[0]}.${date[1]}.${date[2]}`;
     if (type === 'start') {
@@ -48,12 +79,15 @@ const ProjectDuration = () => {
   };
 
   useEffect(() => {
-    console.log(duration);
     const newMarkdown = markdown.map((item: ItemProps) => {
       if (item.name === 'period') {
+        let script = `## 프로젝트 기간: ${duration.start} ~ ${duration.end}\n`;
+        if (!duration.start && !duration.end) {
+          script = `## 프로젝트 기간: \n`;
+        }
         return {
           ...item,
-          detail: `## 프로젝트 기간: ${duration.start} ~ ${duration.end}\n`,
+          detail: script,
         };
       }
       return item;
@@ -69,14 +103,14 @@ const ProjectDuration = () => {
           type="date"
           placeholder="시작한 날짜를 입력하세요. ex) 2***.**.**"
           onChange={(e) => handleChangeDuration(e, DURATION.START)}
-          value={duration.start && duration.start.split('.').join('-')}
+          value={duration.start ? duration.start.split('.').join('-') : ''}
         />
         <img src="/assets/icons/range.svg" />
         <S.Input
           type="date"
           placeholder="완료한 날짜를 입력하세요. ex) 2***.**.**"
           onChange={(e) => handleChangeDuration(e, DURATION.END)}
-          value={duration.end && duration.end.split('.').join('-')}
+          value={duration.end ? duration.end.split('.').join('-') : ''}
         />
       </S.InputContainer>
     </S.InputBox>
